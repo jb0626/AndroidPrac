8e3a0a65caade06e5fe7b6247aaa15968e3e7bdb
jay <jum12345@naver.com>
2023-12-06 02:20:35 +0900

    2023. 12 .06 row, column, box Practice

--- app/src/main/java/per/prac/androidprac/ui/composes/Layouts.kt
+++ app/src/main/java/per/prac/androidprac/ui/composes/Layouts.kt
@@ -1,17 +1,31 @@
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import per.prac.androidprac.R
 
 @Composable
 fun createConstraintBox() {
@@ -56,14 +70,80 @@ fun colLayout() {
 }
 
 @Composable
-@Preview(showBackground = true, showSystemUi = true)
-fun cocLayout() {
+//@Preview(showBackground = true, showSystemUi = true)
+fun rcLayout() {
     Row(
+        horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(text = "HI")
+        Column(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .fillMaxWidth()
+        ) {
+            Text(text = "Hello there", modifier = Modifier.align(Alignment.CenterHorizontally))
+            Text(text = "How r u", modifier = Modifier.align(Alignment.CenterHorizontally))
+            Text(text = "Bye there", modifier = Modifier.align(Alignment.CenterHorizontally))
+        }
+    }
+}
+
+@Composable
+//@Preview(showBackground = true, showSystemUi = true)
+fun cocLayout() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Hello there")
+        Text(text = "How r u")
+        Text(text = "Bye there")
     }
 } // Center of Center
+
+@Composable
+//@Preview(showSystemUi = true, showBackground = true)
+fun bubbleChatLayout() {
+    Row {
+        Box {
+            Image(
+                painter = painterResource(id = R.drawable.baseline_chat_bubble_24),
+                contentDescription = "Chat Bubble"
+            )
+            Box(
+                modifier = Modifier
+                    .background(color = Color.Red, shape = CircleShape)
+                    .width(30.dp)
+                    .height(30.dp)
+                    .align(Alignment.TopEnd)
+            ) {
+                Text(
+                    text = "7",
+                    textAlign = TextAlign.Center,
+                    fontSize = 20.sp,
+                    color = Color.White,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showSystemUi = true, showBackground = true)
+fun boxLayout() {
+    Box(modifier = Modifier.background(color = Color.Gray)) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Android Image"
+        )
+        Icon(
+            Icons.Filled.Search,
+            contentDescription = "Icon",
+            modifier = Modifier.padding(vertical = 46.dp, horizontal = 55.dp)
+        )
+    }
+}
 
 @Composable
 fun createButton(str: String) {
